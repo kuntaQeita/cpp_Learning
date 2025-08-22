d7a7bd6694a59b13857c57bd4967bdb9191a16c5
kuntaQeita <tanzerine445@gmail.com>
2025-08-23 00:17:22 +0530

    comming after adding the fruit bill Question

---
+++ Loops/FruitBill.cpp
@@ -0,0 +1,13 @@
+// Supermarket store: 5 items...
+// Display the items and its price.
+// User can select the item and quantity.
+// 18% CGST and 12% SGST on each item.
+// Calculate the total price and display it.
+
+// 1. Apple - Rs. 100
+// 2. Banana - Rs. 50
+// 3. Orange - Rs. 80
+// 4. Mango - Rs. 120
+// 5. Grapes - Rs. 200 
+
+// Select item and quantity and press 0 0 for shopping complete: 2 20
